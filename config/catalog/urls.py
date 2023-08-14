7c6d52219332f22bfd9f20570139376392bfe53c
Alexandra <alexandrasolovjova@mail.ru>
2023-08-14 23:17:48 +0300

    add view controllers

--- config/catalog/urls.py
+++ config/catalog/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from catalog.apps import CatalogConfig
-from catalog.view import home, contacts
+from catalog.views import home, contacts
 
 
 app_name = CatalogConfig.name
