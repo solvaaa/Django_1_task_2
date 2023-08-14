09df95ce04405077ff3ea80385d50ef9d963d163
Alexandra <alexandrasolovjova@mail.ru>
2023-08-14 23:17:19 +0300

    fixed type

--- config/catalog/views.py
+++ config/catalog/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+def home(request):
+    return render(request, 'catalog/home.html')
+
+
+def contacts(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        message = request.POST.get('message')
+        print(f'You have new message from {name}({email}): {message}')
+    return render(request, 'catalog/contacts.html')
