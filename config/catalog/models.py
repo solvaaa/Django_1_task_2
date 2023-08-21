275da2d81db843f5e67db10902d4ced34f10251d
Alexandra <alexandrasolovjova@mail.ru>
2023-08-21 17:31:56 +0300

    add Product and Category models (Task 2)

--- config/catalog/models.py
+++ config/catalog/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Product(models.Model):
+    pass
+
+    class Meta:
+        verbose_name = 'товар'
+        verbose_name_plural = 'товары'
+
+
+class Category(models.Model):
+    pass
+
+    class Meta:
+        verbose_name = 'категория'
+        verbose_name_plural = 'категории'
