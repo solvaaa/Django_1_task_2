201d0c410c564f70f95d6a4b3e28b62fad008ad1
Alexandra <alexandrasolovjova@mail.ru>
2023-08-21 17:34:47 +0300

    added fields to new models (Task 3)

--- config/catalog/models.py
+++ config/catalog/models.py
@@ -4,7 +4,16 @@ from django.db import models
 
 
 class Product(models.Model):
-    pass
+    name = models.CharField(max_length=100, verbose_name='Название')
+    description = models.TextField(verbose_name='Описание')
+    preview = models.ImageField(verbose_name='Изображение')
+    category = models.ForeignKey("Category", on_delete=models.CASCADE)
+    price = models.IntegerField(verbose_name='Цена')
+    date_created = models.DateField(verbose_name='Создано')
+    date_modified = models.DateField(verbose_name='Изменено')
+
+    def __str__(self):
+        return f'{self.name} {self.category} {self.price}'
 
     class Meta:
         verbose_name = 'товар'
@@ -12,7 +21,8 @@ class Product(models.Model):
 
 
 class Category(models.Model):
-    pass
+    name = models.CharField(max_length=100, verbose_name='Название')
+    description = models.TextField(verbose_name='Описание')
 
     class Meta:
         verbose_name = 'категория'
