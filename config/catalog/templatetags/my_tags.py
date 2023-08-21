5c360bb6b74c7d38f01336d7c2669c9d0554976b
Alexandra <alexandrasolovjova@mail.ru>
2023-08-21 22:07:46 +0300

    use MEDIA_URL setting for filter tag

--- config/catalog/templatetags/my_tags.py
+++ config/catalog/templatetags/my_tags.py
@@ -1,8 +1,9 @@
 from django import template
-
+from config.settings import MEDIA_URL
 register = template.Library()
+
 
 @register.filter(needs_autoescape=True)
 def mediapath(text, autoescape=True):
-    media_path = f'/media/{text}'
+    media_path = f'{MEDIA_URL}{text}'
     return media_path
