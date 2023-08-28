479386de7861f7995da16282bf3d38448d8d944e
Alexandra <alexandrasolovjova@mail.ru>
2023-08-28 19:57:37 +0300

    add blogpost detail view

--- config/catalog/urls.py
+++ config/catalog/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from catalog.apps import CatalogConfig
 from catalog.views import ContactsView, ProductListView, ProductDetailView
-from catalog.views import BlogPostListView
+from catalog.views import BlogPostListView, BlogPostDetailView
 
 
 app_name = CatalogConfig.name
@@ -9,6 +9,7 @@ app_name = CatalogConfig.name
 
 urlpatterns = [
     path('', BlogPostListView.as_view(), name='home'),
+    path('/blog/<int:pk>/', BlogPostDetailView.as_view(), name='blogpost'),
     path('contacts/', ContactsView.as_view(), name='contacts'),
     path('catalog/', ProductListView.as_view(), name='catalog'),
     path('product/<int:pk>/', ProductDetailView.as_view(), name='product'),
--- config/catalog/views.py
+++ config/catalog/views.py
@@ -30,3 +30,7 @@ class ProductDetailView(DetailView):
 
 class BlogPostListView(ListView):
     model = BlogPost
+
+
+class BlogPostDetailView(DetailView):
+    model = BlogPost
