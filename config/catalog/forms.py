68d4b80c58ddfb1f1580e15910390d13fad63d22
Alexandra <alexandrasolovjova@mail.ru>
2023-09-10 22:17:28 +0300

    add name and description clean in ProductForm

--- config/catalog/forms.py
+++ config/catalog/forms.py
@@ -7,3 +7,35 @@ class ProductForm(forms.ModelForm):
     class Meta:
         model = Product
         fields = '__all__'
+
+    def check_restricted_words(self, text):
+        restricted_words = [
+            'казино',
+            'криптовалюта',
+            'крипта',
+            'биржа',
+            'дешево',
+            'бесплатно',
+            'обман',
+            'полиция',
+            'радар']
+        for word in restricted_words:
+            if word in text.lower():
+                return True
+            return False
+
+    def clean_name(self):
+        name = self.cleaned_data['name']
+
+        if self.check_restricted_words(name):
+            raise forms.ValidationError('Запрещенное слово в названии товара')
+
+        return name
+
+    def clean_description(self):
+        description = self.cleaned_data['description']
+
+        if self.check_restricted_words(description):
+            raise forms.ValidationError('Запрещенное слово в описании товара')
+
+        return description
