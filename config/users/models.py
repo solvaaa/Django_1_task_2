624729f0c55f5d92fa59b9808d8637d06a238548
Alexandra <alexandrasolovjova@mail.ru>
2023-09-11 19:22:34 +0300

    remigrate with users model

--- config/users/models.py
+++ config/users/models.py
@@ -1,3 +1,16 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 # Create your models here.
+
+
+class User(AbstractUser):
+    username = None
+    email = models.EmailField(unique=True, verbose_name='email')
+
+    phone = models.CharField(max_length=35, verbose_name='телефон', blank=True, null=True)
+    avatar = models.ImageField(upload_to='users/', verbose_name='аватар', blank=True, null=True)
+    country = models.CharField(max_length=15, verbose_name='страна', blank=True, null=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
