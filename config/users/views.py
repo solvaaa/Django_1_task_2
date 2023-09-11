e0837ab5e4686c7bcb3572c6ca5c64fd3e83c7f5
Alexandra <alexandrasolovjova@mail.ru>
2023-09-11 20:18:07 +0300

    add user login, logout, register and profile update forms

--- config/users/views.py
+++ config/users/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import CreateView, UpdateView
+from users.models import User
 
-# Create your views here.
+from users.forms import UserRegisterForm
+
+from users.forms import UserProfileForm
+
+
+class RegisterView(CreateView):
+    model = User
+    form_class = UserRegisterForm
+    template_name = 'users/register.html'
+    success_url = reverse_lazy('users:login')
+
+
+class ProfileView(UpdateView):
+    model = User
+    form_class = UserProfileForm
+    success_url = reverse_lazy('users:profile')
+
+    def get_object(self, queryset=None):
+        return self.request.user
