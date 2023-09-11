fab4757792df378928012874641b714c3165affe
Alexandra <alexandrasolovjova@mail.ru>
2023-09-11 23:41:22 +0300

    fix ProductForm

--- config/catalog/forms.py
+++ config/catalog/forms.py
@@ -7,9 +7,10 @@ class ProductForm(forms.ModelForm):
     class Meta:
         model = Product
         fields = '__all__'
+        exclude = ('user', )
 
     def __init__(self, *args, **kwargs):
-        super().__init__()
+        super().__init__(*args, **kwargs)
         for field_name, field in self.fields.items():
             if field_name != 'is_active':
                 field.widget.attrs['class'] = 'form-control'
@@ -47,6 +48,7 @@ class ProductForm(forms.ModelForm):
             raise forms.ValidationError('Запрещенное слово в описании товара')
 
         return description
+
 
 class VersionForm(forms.ModelForm):
 
