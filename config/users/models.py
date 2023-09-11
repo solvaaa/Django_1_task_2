ac03e5c8197ffd4b09a3f92201e81ada9965fa59
Alexandra <alexandrasolovjova@mail.ru>
2023-09-11 20:29:06 +0300

    add is_verified field to User model

--- config/users/models.py
+++ config/users/models.py
@@ -11,6 +11,7 @@ class User(AbstractUser):
     phone = models.CharField(max_length=35, verbose_name='телефон', blank=True, null=True)
     avatar = models.ImageField(upload_to='users/', verbose_name='аватар', blank=True, null=True)
     country = models.CharField(max_length=15, verbose_name='страна', blank=True, null=True)
+    is_verified = models.BooleanField(verbose_name='подтверждён', default=False)
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
