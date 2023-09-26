ef78e798d3e0d77b478ff0d4d2afa46cb52e39d2
Alexandra <alexandrasolovjova@mail.ru>
2023-09-26 13:21:16 +0300

    add category function with cache

---
+++ config/catalog/services.py
@@ -0,0 +1,16 @@
+from catalog.models import Category
+import settings
+from django.core.cache import cache
+
+
+def get_categories():
+    if settings.CACHE_ENABLED:
+        key = 'category_list'
+        category_list = cache.get(key)
+        if category_list is None:
+            category_list = Category.objects.all()
+            cache.set(key, category_list)
+    else:
+        category_list = Category.objects.all()
+
+    return category_list
